5a3b751895819ed0855dfb3c8a8b18aeaf7a5772
Arthur Fedotiev <artur.fedotiew@gmail.com>
2021-11-23 18:00:05 +0200

    added firebase

--- apps/fitness-tracker/src/app/app.module.ts
+++ apps/fitness-tracker/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
@@ -9,11 +10,22 @@ import { environment } from '../environments/environment';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from '@fitness-tracker/auth';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule,
     StoreModule.forRoot(
       {},
@@ -29,8 +41,20 @@ import { AuthModule } from '@fitness-tracker/auth';
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     AppRoutingModule,
     AuthModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage()),
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- apps/fitness-tracker/src/environments/environment.ts
+++ apps/fitness-tracker/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'fitness-tracker-de06b',
+    appId: '1:450961916900:web:a85ab7b9651ed92cd5bdf1',
+    storageBucket: 'fitness-tracker-de06b.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCae4RTHeovIDsippQzDnGFUhgExauhKv8',
+    authDomain: 'fitness-tracker-de06b.firebaseapp.com',
+    messagingSenderId: '450961916900',
+  },
   production: false,
 };
 
