c743cac148930343f165e904e01e0a3241cce2c8
Arthur Fedotiev <artur.fedotiew@gmail.com>
2021-11-23 17:59:55 +0200

    added material module

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { LoginComponent } from './containers/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { MaterialModule } from "@fitness-tracker/material";
 
 export const authRoutes: Route[] = [
   { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MaterialModule],
   declarations: [
     LoginComponent,
     LoginFormComponent
