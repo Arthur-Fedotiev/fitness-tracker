7d5bd123d8d584c95b9c3493ea20bfdf2b4f1630
Arthur Fedotiev <artur.fedotiew@gmail.com>
2021-11-26 16:33:16 +0200

    layout refactored

--- libs/layout/feature/src/lib/layout-feature.module.ts
+++ libs/layout/feature/src/lib/layout-feature.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
-  import { LayoutUiModule } from '@fitness-tracker/layout/ui';
+import { LayoutUiModule } from '@fitness-tracker/layout/ui';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, LayoutUiModule],
-declarations: [
+  imports: [CommonModule, LayoutUiModule, RouterModule],
+  declarations: [
     LayoutComponent
   ],
   exports: [
     LayoutComponent
   ],
 })
-export class LayoutFeatureModule {}
+export class LayoutFeatureModule { }
