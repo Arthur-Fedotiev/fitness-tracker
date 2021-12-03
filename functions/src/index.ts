442540bbf23f2780f53d6f9181c4e79527fd8f91
Arthur Fedotiev <artur.fedotiew@gmail.com>
2021-12-03 17:10:33 +0200

    added cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,14 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+export const onAddAdminUser =
+  functions
+    .runWith({
+      timeoutSeconds: 300,
+      memory: "128MB"
+    })
+    .firestore.document("users/{userId}")
+    .onCreate(async (snap, context) => {
+      await (
+        await import("./create-admin"))
+        .default(snap, context);
+    });
