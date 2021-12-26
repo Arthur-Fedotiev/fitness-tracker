fb2b1cfc03b303eb7f4389d87617b4402c57952e
Arthur Fedotiev <artur.fedotiew@gmail.com>
2021-12-26 09:38:49 +0200

    (feat) refactored workout-load

--- libs/shared/dialogs/src/lib/compose-workout/components/workout-item-load-subform/workout-item-load-subform.component.ts
+++ libs/shared/dialogs/src/lib/compose-workout/components/workout-item-load-subform/workout-item-load-subform.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { ControlContainer } from '@angular/forms';
-import { InstructionType } from '../../compose-workout.component';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { formViewProvider } from '@fitness-tracker/shared/utils';
+import {
+  InstructionType,
+  WorkoutItemInstruction,
+} from '../../compose-workout.component';
 
 @Component({
-  selector: 'ft-workout-item-load-subform',
+  selector: 'ft-workout-item-load',
   templateUrl: './workout-item-load-subform.component.html',
   styleUrls: ['./workout-item-load-subform.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  viewProviders: [formViewProvider],
 })
-export class WorkoutItemLoadSubformComponent implements OnInit {
+export class WorkoutItemLoadSubformComponent {
+  @Input() instruction!: WorkoutItemInstruction;
   public readonly instructionType = InstructionType;
-  constructor(public readonly controlContainer: ControlContainer) {
-    console.log(controlContainer);
-  }
-
-  ngOnInit(): void {}
 }
