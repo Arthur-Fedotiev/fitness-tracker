40febc0d61527eb7a6527f060d744b372abb2dd7
Arthur Fedotiev <artur.fedotiew@gmail.com>
2022-01-04 18:41:59 +0200

    (feat) implemented workout-details component with use of cards for layout

--- libs/workout/feature/src/lib/workout-details/workout-details.module.ts
+++ libs/workout/feature/src/lib/workout-details/workout-details.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { WorkoutDetailsComponent } from './workout-details.component';
 import { WorkoutDetailsRoutingModule } from './workout-details-routing.module';
 import { MaterialModule } from '@fitness-tracker/shared-ui-material';
+import { ImgFallbackModule } from '@fitness-tracker/shared/utils';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [WorkoutDetailsComponent],
-  imports: [CommonModule, WorkoutDetailsRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    WorkoutDetailsRoutingModule,
+    MaterialModule,
+    ImgFallbackModule,
+    FlexLayoutModule,
+  ],
   exports: [WorkoutDetailsComponent],
 })
 export class WorkoutDetailsModule {}
